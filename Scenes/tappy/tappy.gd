7820ba0db6a054667076519e350041603310c01b
Chrysovalantis Siomos <SIOPIS2008@HOTMAIL.COM>
2026-09-03 21:02:18 +0300

    jump, collision, top and down BoundaryShape2D with acorrect faces

---
+++ Scenes/tappy/tappy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const JUMP_FORCE: float = -350.0
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+var _jumped: bool = false
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("fly"):
+		_jumped = true
+
+func _physics_process(delta: float) -> void:
+	velocity.y += _gravity * delta
+	if _jumped:
+		velocity.y = JUMP_FORCE
+		_jumped = false
+	
+	move_and_slide()
