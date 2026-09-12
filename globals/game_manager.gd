2f685562f289deef4f22e15ce4ae6f2d1ceb3ea9
Chrysovalantis Siomos <SIOPIS2008@HOTMAIL.COM>
2026-09-12 08:22:01 +0300

    loading screen between main and game screen

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -2,9 +2,30 @@ extends Node
 
 const GAME = preload("uid://bnqilomf5vcb8")
 const  MAIN = preload("uid://bk7wx4bliuk8n")
+const LOADING_SCREEN = preload("uid://1terrn8vuvs5")
+
+#region Priginal
+
+#func load_game_screen() -> void:
+	#get_tree().change_scene_to_packed(GAME)
+#
+#func load_main_screen() -> void:
+	#get_tree().change_scene_to_packed(MAIN)
+
+
+#region Simple Change
+
+var _next_scene: PackedScene
+
+func change_to_next() -> void:
+	get_tree().change_scene_to_packed(_next_scene)
 
 func load_game_screen() -> void:
-	get_tree().change_scene_to_packed(GAME)
+	_next_scene = GAME
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
 
 func load_main_screen() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	_next_scene = MAIN
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
+
+#endregion
